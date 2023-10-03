3d14055f408127ca2e14aea693419e2848919ec0
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-10-03 15:24:03 +0500

    About us

--- src/pages/Aboutus/index.tsx
+++ src/pages/Aboutus/index.tsx
@@ -5,10 +5,81 @@ import CartColumnframe48095972 from "components/CartColumnframe48095972";
 import CartNavbar from "components/CartNavbar";
 import CartSectionfooter from "components/CartSectionfooter";
 import TeamCardteam from "components/TeamCardteam";
+import HomepageCardproduct from "components/HomepageCardproduct";
 
 const AboutusPage: React.FC = () => {
   const sliderRef = React.useRef(null);
   const [sliderState, setsliderState] = React.useState(0);
+  const homepageCardproductPropList = [
+    { save: "images/products/5.png", status: "New" },
+    {
+      image: "images/products/1.png",
+      category: "4x Rooms",
+      teakwoodchair: "house building",
+      twentyfour: "$220.000",
+    },
+    {
+      image: "images/products/2.png",
+      category: "4x Rooms",
+      teakwoodchair: "house building",
+      twentyfour: "$330.000",
+    },
+
+    {
+      image: "images/products/second/Frame 13.png",
+    },
+    {
+      image: "images/products/second/Frame 14.png",
+    },
+    {
+      image: "images/products/second/Frame 15.png",
+    },
+    {
+      image: "images/products/3.png",
+      category: "2x Rooms",
+      teakwoodchair: "house building",
+      twentyfour: "$260.000",
+    },
+    {
+      image: "images/products/4.png",
+      category: "6x Rooms",
+      teakwoodchair: "house building",
+      twentyfour: "$280.000",
+    },
+    {
+      image: "images/products/5.png",
+      category: "Rooms building",
+      teakwoodchair: "house building",
+      twentyfour: "$108.000",
+    },
+    {
+      image: "images/products/second/Frame 16.png",
+    },
+    {
+      image: "images/products/second/Frame 17.png",
+    },
+    {
+      image: "images/products/second/Frame 18.png",
+    },
+    {
+      image: "images/products/6.png",
+      category: "Rooms building",
+      teakwoodchair: "house building",
+      twentyfour: "$162.000",
+    },
+    {
+      image: "images/products/7.png",
+      category: "Rooms building",
+      teakwoodchair: "house building",
+      twentyfour: "$120.000",
+    },
+    {
+      image: "images/products/2.png",
+      category: "Rooms building",
+      teakwoodchair: "house building",
+      twentyfour: "$189.000",
+    },
+  ];
 
   return (
     <>
@@ -20,25 +91,24 @@ const AboutusPage: React.FC = () => {
               <div className="flex flex-col gap-[25px] items-start justify-start w-full">
                 <div className="flex flex-col gap-[25px] items-start justify-start w-full">
                   <Text
-                    className="leading-[60.00px] max-w-[626px] md:max-w-full text-5xl sm:text-[38px] md:text-[44px] text-black-900 tracking-[-0.50px]"
+                    className="leading-[60.00px] max-w-[656px] md:max-w-full text-5xl sm:text-[38px] md:text-[44px] text-black-900 tracking-[-0.50px]"
                     size="txtRalewayRomanBold48"
                   >
-                    Provide the best quality ingredients for home products
+                    Разрабатывая проект 7 Residence
                   </Text>
                   <Text
                     className="leading-[35.00px] max-w-[626px] md:max-w-full text-base text-gray-500 tracking-[-0.50px]"
                     size="txtRubikRegular16"
                   >
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit
-                    dictumst posuere, lectus dis vehicula augue elementum quam
-                    risus. Placerat dictum lobortis lacinia volutpat morbi cum
-                    justo commodo est aliquam, facilisi consequat ligula vivamus
-                    faucibus
+                    Двор жилого комплекса - это защищенна территория, где всё
+                    продуманно для комфортной жизни. Созданы условия для занятий
+                    спортом, а ваши дети смогут игра безопасности, во дворе без
+                    машин, благо) системе car-free zone. Машины, кроме пожарной
+                    и медицинской, не имеют досту на территорию двора. Для
+                    автомобилей жильцов создано специальная многоуровневая
+                    парковочная зона.
                   </Text>
                 </div>
-                <Button className="border border-black-900 border-solid cursor-pointer font-semibold leading-[normal] min-w-[172px] py-[11px] text-black-900 text-center text-lg tracking-[-0.50px]">
-                  Meet our Team
-                </Button>
               </div>
               <div className="flex flex-col gap-[22px] items-start justify-start w-full"></div>
             </div>
@@ -49,60 +119,21 @@ const AboutusPage: React.FC = () => {
             />
           </div>
         </div>
-        <div className="bg-black-900 flex flex-col items-start justify-start md:px-10 sm:px-5 px-[173px] py-[41px] w-full"></div>
+        <div className="bg-black-900 flex flex-col items-start justify-start md:px-10 sm:px-5 px-[173px] py-[11px] w-full"></div>
 
         <div className="flex flex-col items-start justify-start md:px-10 sm:px-5 px-[75px] w-full">
           <div className="flex flex-col gap-[53px] items-center justify-start max-w-[1290px] mx-auto w-full">
-            <div className="flex flex-col gap-[13px] items-center justify-start w-full">
-              <Text
-                className="sm:text-4xl md:text-[38px] text-[40px] text-black-900 text-center tracking-[-0.50px] w-full"
-                size="txtRalewayBold40"
-              >
-                Meet Our Team
-              </Text>
-              <Text
-                className="text-center text-gray-500 text-lg tracking-[-0.50px] w-full"
-                size="txtRubikRegular18Gray500"
-              >
-                We write various things related to furniture, from tips and what
-                things I need to pay attention to when choosing furniture
-              </Text>
-            </div>
-            <div className="flex flex-col items-start justify-start w-full">
-              <Slider
-                autoPlay
-                autoPlayInterval={2000}
-                activeIndex={sliderState}
-                responsive={{
-                  0: { items: 1 },
-                  550: { items: 2 },
-                  1050: { items: 3 },
-                }}
-                onSlideChanged={(e) => {
-                  setsliderState(e?.item);
-                }}
-                ref={sliderRef}
-                className="gap-5 w-full"
-                items={[...Array(9)].map(() => (
-                  <React.Fragment key={Math.random()}>
-                    <TeamCardteam className="flex flex-1 flex-col gap-2 items-start justify-start mx-2.5" />
+            <div className="flex flex-col items-center justify-start w-full">
+              <div className="gap-5 grid sm:grid-cols-1 md:grid-cols-2 grid-cols-3 justify-center min-h-[auto] w-full">
+                {homepageCardproductPropList.map((props, index) => (
+                  <React.Fragment key={`HomepageCardproduct${index}`}>
+                    <HomepageCardproduct
+                      className="flex flex-1 flex-col gap-4 items-start justify-start w-full"
+                      {...props}
+                    />
                   </React.Fragment>
                 ))}
-                renderDotsItem={({ isActive }) => {
-                  if (isActive) {
-                    return (
-                      <div className="inline-block cursor-pointer rounded-[50%] h-[15px] bg-bluegray-900 w-[15px]" />
-                    );
-                  }
-                  return (
-                    <div
-                      className="inline-block cursor-pointer rounded-[50%] h-[15px] bg-gray-200 w-[15px]"
-                      role="button"
-                      tabIndex={0}
-                    />
-                  );
-                }}
-              />
+              </div>
             </div>
             <PagerIndicator
               className="flex gap-[15px] h-[15px] items-start justify-start w-[75px]"
@@ -122,16 +153,16 @@ const AboutusPage: React.FC = () => {
             <div className="flex sm:flex-1 flex-col gap-[30px] items-start justify-start w-[525px] sm:w-full">
               <div className="flex flex-col gap-[18px] items-start justify-start w-full">
                 <Text
-                  className="text-2xl md:text-[22px] text-bluegray-900 sm:text-xl tracking-[-0.50px] w-full"
+                  className="text-2xl md:text-[22px] text-bluegray-900 sm:text-xl tracking-[-0.50px] w-full ml-10"
                   size="txtRubikRegular24"
                 >
-                  Interior Modern
+                  Интерьер Модерн
                 </Text>
                 <Text
-                  className="leading-[60.00px] max-w-[525px] md:max-w-full sm:text-4xl md:text-[38px] text-[40px] text-black-900 tracking-[-0.50px]"
+                  className="leading-[60.00px] max-w-[525px] md:max-w-full sm:text-4xl md:text-[38px] text-[40px] text-black-900 tracking-[-0.50px] ml-10"
                   size="txtRalewayRomanBold40"
                 >
-                  Arrange your home in such a way with our modern interiors
+                  Обустройте свой дом таким образом с наших современных
                 </Text>
               </div>
             </div>
