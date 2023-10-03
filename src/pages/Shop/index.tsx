2d15feb04a9ecb08b260d5454f8e06b39e5df95d
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-10-03 11:52:53 +0500

    create new products data

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -19,15 +19,55 @@ const sortOptionsList = [
 
 const ShopPage: React.FC = () => {
   const homepageCardproductPropList = [
-    { save: "images/img_save.svg", status: "New" },
-    { image: "images/img_image_7.png" },
-    { image: "images/img_image_8.png" },
-    { image: "images/img_image_10.png" },
-    { image: "images/img_image_11.png" },
-    { image: "images/img_image_12.png" },
-    { image: "images/img_image_9.png" },
-    { image: "images/img_image_13.png" },
-    { image: "images/img_image_7.png" },
+    { save: "images/products/5.png", status: "New" },
+    {
+      image: "images/products/1.png",
+      category: "4x Rooms",
+      teakwoodchair: "house building",
+      twentyfour: "$220.000",
+    },
+    {
+      image: "images/products/2.png",
+      category: "4x Rooms",
+      teakwoodchair: "house building",
+      twentyfour: "$330.000",
+    },
+    {
+      image: "images/products/3.png",
+      category: "2x Rooms",
+      teakwoodchair: "house building",
+      twentyfour: "$260.000",
+    },
+    {
+      image: "images/products/4.png",
+      category: "6x Rooms",
+      teakwoodchair: "house building",
+      twentyfour: "$280.000",
+    },
+    {
+      image: "images/products/5.png",
+      category: "Rooms building",
+      teakwoodchair: "house building",
+      twentyfour: "$108.000",
+    },
+    {
+      image: "images/products/6.png",
+      category: "Rooms building",
+      teakwoodchair: "house building",
+      twentyfour: "$162.000",
+    },
+    {
+      image: "images/products/7.png",
+      category: "Rooms building",
+      teakwoodchair: "house building",
+      twentyfour: "$120.000",
+    },
+    {
+      image: "images/products/2.png",
+      category: "Rooms building",
+      teakwoodchair: "house building",
+      twentyfour: "$189.000",
+    },
   ];
 
   return (
@@ -66,9 +106,10 @@ const ShopPage: React.FC = () => {
             </div>
           </div>
         </div>
+        {/* ////////// */}
         <div className="flex flex-col font-raleway items-start justify-start md:px-10 sm:px-5 px-[75px] w-full">
           <div className="flex md:flex-col flex-row gap-5 items-start justify-start max-w-[1290px] mx-auto w-full">
-            <div className="flex flex-col md:gap-10 gap-[60px] items-start justify-start w-[308px]">
+            {/* <div className="flex flex-col md:gap-10 gap-[60px] items-start justify-start w-[308px]">
               <div className="flex flex-col gap-[22px] items-start justify-start w-full">
                 <Text
                   className="text-black-900 text-xl w-full"
@@ -225,7 +266,7 @@ const ShopPage: React.FC = () => {
                   </Button>
                 </div>
               </div>
-            </div>
+            </div> */}
             <div className="flex flex-1 flex-col font-rubik gap-[50px] items-center justify-start w-full">
               <div className="flex sm:flex-col flex-row sm:gap-10 items-center justify-between w-full">
                 <div className="flex sm:flex-1 flex-col font-rubik items-start justify-start w-[407px] sm:w-full">
@@ -260,6 +301,8 @@ const ShopPage: React.FC = () => {
                   placeholder="Sort By"
                 />
               </div>
+
+              {/* ////////// */}
               <div className="flex flex-col items-center justify-start w-full">
                 <div className="gap-5 grid sm:grid-cols-1 md:grid-cols-2 grid-cols-3 justify-center min-h-[auto] w-full">
                   {homepageCardproductPropList.map((props, index) => (
@@ -272,7 +315,9 @@ const ShopPage: React.FC = () => {
                   ))}
                 </div>
               </div>
-              <div className="flex flex-row gap-2.5 items-center justify-center max-w-[962px] w-full">
+              {/* ////////// */}
+
+              {/* <div className="flex flex-row gap-2.5 items-center justify-center max-w-[962px] w-full">
                 <Img
                   className="h-[15px] w-[15px]"
                   src="images/img_arrowleft.svg"
@@ -295,13 +340,15 @@ const ShopPage: React.FC = () => {
                   src="images/img_arrowright.svg"
                   alt="arrowright"
                 />
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
+        {/* ////////// */}
         <div className="flex flex-col font-rubik items-start justify-start md:px-10 sm:px-5 px-[75px] w-full">
           <CartColumnframe48095972 className="bg-gradient  flex flex-col gap-2 items-start justify-start max-w-[1290px] mx-auto pl-[59px] md:px-5 py-[46px] w-full" />
         </div>
+
         <CartSectionfooter className="bg-black-900 flex font-raleway gap-2 items-center justify-center md:px-5 px-[75px] py-[50px] w-full" />
       </div>
     </>
