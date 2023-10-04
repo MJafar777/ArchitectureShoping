af87e1b2f673d321e417460aa33b137c24f1f69e
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-10-04 17:58:52 +0500

    slider add in  page of room

--- src/components/ThumbSlide/generateSlides.ts
+++ src/components/ThumbSlide/generateSlides.ts
@@ -1,3 +1,5 @@
+import { room11, room12, room13, room14, room15 } from "assets/room";
+import img from "../../assets/room/room11.jpg";
 /**
  * Return an array with objects containing data of sample images.
  *
@@ -6,13 +8,59 @@
  *
  * @return An array with objects for sample images.
  */
-export function generateSlides( length = 10, sig = 0 ): Array<{ src: string, alt: string }> {
-    return Array.from( { length } ).map( ( value, index ) => {
-      index = sig || index;
-  
-      return {
-        src: `https://source.unsplash.com/random/800x450?sig=${ index }`,
-        alt: `Image ${ index + 1 }`,
-      };
-    } );
-  }
+export function generateSlides(
+  length = 8,
+  sig = 0
+): Array<{ src: string; alt: string }> {
+  return Array.from({ length }).map((value, index) => {
+    index = sig || index;
+    // let room1 = "room";
+    return {
+      src: `${room11}`,
+      alt: `Image ${index + 1}`,
+    };
+    // switch (index) {
+    //   case 1:
+
+    //   case 2:
+    //     return {
+    //       src: `${room12}`,
+    //       alt: `Image ${index + 1}`,
+    //     };
+
+    //   case 3:
+    //     return {
+    //       src: `${room13}`,
+    //       alt: `Image ${index + 1}`,
+    //     };
+
+    //   case 4:
+    //     return {
+    //       src: `${room14}`,
+    //       alt: `Image ${index + 1}`,
+    //     };
+    //   case 5:
+    //     return {
+    //       src: `${room15}`,
+    //       alt: `Image ${index + 1}`,
+    //     };
+    //   case 6:
+    //     return {
+    //       src: `${room11}`,
+    //       alt: `Image ${index + 1}`,
+    //     };
+
+    //   case 7:
+    //     return {
+    //       src: `${room12}`,
+    //       alt: `Image ${index + 1}`,
+    //     };
+
+    //   case 8:
+    //     return {
+    //       src: `${room13}`,
+    //       alt: `Image ${index + 1}`,
+    //     };
+    // }
+  });
+}
