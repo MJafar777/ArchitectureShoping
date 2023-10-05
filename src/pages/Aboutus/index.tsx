5ca3ffcff415cc2baae08548fba18ee1fba2126d
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-10-05 10:49:42 +0500

    Fixed Footer

--- src/pages/Aboutus/index.tsx
+++ src/pages/Aboutus/index.tsx
@@ -76,6 +76,7 @@ const AboutusPage: React.FC = () => {
         <div className="flex flex-col font-rubik items-start justify-start md:px-10 sm:px-5 px-[75px] w-full">
           <CartColumnframe48095972 className="bg-gradient  flex flex-col gap-2 items-start justify-start max-w-[1290px] mx-auto pl-[59px] md:px-5 py-[46px] w-full" />
         </div>
+
         <CartSectionfooter className="bg-black-900 flex font-raleway gap-2 items-center justify-center md:px-5 px-[75px] py-[50px] w-full" />
       </div>
     </>
